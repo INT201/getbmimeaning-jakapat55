6d29222928db3af3afe019c8f3de5cac191865df
jakapat <jakapat@mail.kmutt.ac.th>
2022-09-07 23:15:53 +0700

    finish

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,17 @@
 const { template } = require('@babel/core')
-let weight = 10
+
 function calculateBMI(weight, height) {
   //code here
+  return weight/(height^2)
 }
 function getBMIMeaning(weight, height) {
   //code here
+  let bmi = calculateBMI(weight, height) 
+  if(bmi>25.0) return "Overweight"
+  if(bmi<18.5) return "Underweight"
+  else return "Normal weight"
+  
+
 }
+console.log(getBMIMeaning(200,2))
 module.exports = getBMIMeaning
